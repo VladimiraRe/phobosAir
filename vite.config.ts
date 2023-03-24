32cd06f9fabce038751eb01b087dafecbc0a0454
levotarayan98 <levotarayan98@gmail.com>
2023-03-24 13:43:22 +0500

    init commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), react(), eslint()],
+  server: {
+    port: 4000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
+    deps: {
+      fallbackCJS: true,
+    },
+  },
+});
